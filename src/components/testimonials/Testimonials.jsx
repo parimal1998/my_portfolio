38269648b311818c552858a6fd74701d21a35747
parimal <parimalsawant0@gmail.com>
2025-11-28 22:47:30 +0000

    testimonial section edit

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -14,7 +14,7 @@ import { Pagination } from "swiper";
 
 const Testimonials = () => {
     return (
-        <section className="testimonial container section">
+        <section id="testimonials" className="testimonial container section">
             <h2 className="section__title">My colleague say</h2>
             <span className="section__subtitle">Testimonial</span>
 
